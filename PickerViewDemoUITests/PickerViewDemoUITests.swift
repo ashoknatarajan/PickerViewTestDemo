1ac5ace182f3e76bdd0a7e947a015f6100e02cc0
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-07-07 09:21:23 +1000

    Add picker view

--- PickerViewDemoUITests/PickerViewDemoUITests.swift
+++ PickerViewDemoUITests/PickerViewDemoUITests.swift
@@ -1,35 +1,13 @@
-//
-//  PickerViewDemoUITests.swift
-//  PickerViewDemoUITests
-//
-//  Created by Evgenii on 7/07/2015.
-//  Copyright © 2015 Marketplacer. All rights reserved.
-//
-
-import Foundation
 import XCTest
 
 class PickerViewDemoUITests: XCTestCase {
-        
-    override func setUp() {
-        super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-    }
+  func testExample() {
+    let app = XCUIApplication()
+    app.launch()
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    // I can swipe the picker view, but how can I select a **specific** item from it?
+    app.pickers.elementAtIndex(0).swipeUp()
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
+    XCTAssert(app.staticTexts["Selected: Black"].exists)
+  }
 }
