d4a9c6243a53aeb2307e015f6b7890a14ef1766d
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-07-07 09:06:01 +1000

    Add picker view

--- PickerViewDemo/ViewController.swift
+++ PickerViewDemo/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+  @IBOutlet weak var pickerView: UIPickerView!
+  
+  let pickerViewItems = ["Red", "Green", "Yellow", "Black"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  // MARK: UIPickerViewDelegate
+  // ----------------------
+  
+  func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    if row < pickerViewItems.count {
+      return pickerViewItems[row]
+    } else {
+      return ""
+    }
   }
-
-
+  
+  func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    
+  }
+  
+  // MARK: UIPickerViewDataSource
+  // ----------------------
+  
+  func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return pickerViewItems.count
+  }
 }
 
